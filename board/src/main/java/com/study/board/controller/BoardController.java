46693e8123c51c0183ff28a789233191ac46e755
ghi512 <77954741+ghi512@users.noreply.github.com>
2022-11-18 05:07:23 +0900

    게시글 삭제, 수정, 메시지, 파일, 페이징 처리, 검색기능 추가

--- board/src/main/java/com/study/board/controller/BoardController.java
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -3,11 +3,16 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.criteria.CriteriaBuilder;
 
@@ -23,16 +28,37 @@ public class BoardController {
   }
 
   @PostMapping("board/writepro") // 여기 url과 'boardwrite.html' 파일의 form action의 주소가 같아야한다.
-  public String boardWritePro(Board board, Model model) {
-    boardService.write(board);
+  public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception{
+    boardService.write(board, file);
     model.addAttribute("message", "글 작성이 완료되었습니다.");
     model.addAttribute("searchUrl", "/board/list");
     return "message";
   }
 
   @GetMapping("/board/list")
-  public String boardlist(Model model) {
-    model.addAttribute("list", boardService.boardList());
+  public String boardlist(Model model,
+                          @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+                          String searchKeyword) {
+    Page<Board> list = null;
+
+    if(searchKeyword == null) {
+      list = boardService.boardList(pageable);
+    }
+    else {
+      list = boardService.boardSearchList(searchKeyword, pageable);
+    }
+
+
+
+    int nowPage = list.getPageable().getPageNumber() + 1;
+    int startPage = Math.max(nowPage - 4, 1);
+    int endPage = Math.min(nowPage + 5, list.getTotalPages());
+
+    model.addAttribute("list", list);
+    model.addAttribute("nowPage", nowPage);
+    model.addAttribute("startPage", startPage);
+    model.addAttribute("endPage", endPage);
+
     return "boardlist";
   }
 
@@ -56,11 +82,11 @@ public class BoardController {
   }
 
   @PostMapping("/board/update/{id}")
-  public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model) {
+  public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model, MultipartFile file) throws Exception{
     Board boardTemp = boardService.boardView(id);
     boardTemp.setTitle(board.getTitle());
     boardTemp.setContent(board.getContent());
-    boardService.write(boardTemp);
+    boardService.write(boardTemp, file);
     model.addAttribute("message", "글 수정이 완료되었습니다.");
     model.addAttribute("searchUrl", "/board/list");
     return "message";
